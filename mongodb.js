d215a17729a11db670b25d3d27870a129398a2af
EmericReactJS <emeric555@gmail.com>
2021-02-18 09:32:51 +0100

    sets crud functions

--- mongodb.js
+++ mongodb.js
@@ -7,11 +7,41 @@ const client = new mongodb.MongoClient(`${process.env.MONGODB_URI}`, {
   useUnifiedTopology: true
 });
 
-client.connect().then(
-  () => console.log('db connected'),
-  (err) => console.log(err)
-);
+const find = async (collection) => {
+  try {
+    await client.connect();
+    const db = client.db(process.env.MONGODB_DB_NAME);
+    const document = await db.collection(collection).find({}).toArray();
+    return document;
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+};
 
-const db = client.db(process.env.MONGODB_DB_NAME);
+const create = async (collection, body) => {
+  try {
+    await client.connect();
+    const db = client.db(process.env.MONGODB_DB_NAME);
+    await db.collection(collection).insertOne(body);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+};
 
-export default db;
+const del = async (collection, id) => {
+  try {
+    await client.connect();
+    const db = client.db(process.env.MONGODB_DB_NAME);
+    await db.collection(collection).deleteOne(id);
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    await client.close();
+  }
+};
+
+export { find, create, del };
