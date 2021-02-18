caf4eb40db196d0d7f93f9f714bfe34ec55c2f56
EmericReactJS <emeric555@gmail.com>
2021-02-18 14:23:42 +0100

    [fix]multiple client connection

--- mongodb.js
+++ mongodb.js
@@ -7,41 +7,38 @@ const client = new mongodb.MongoClient(`${process.env.MONGODB_URI}`, {
   useUnifiedTopology: true
 });
 
-const find = async (collection) => {
+let db;
+
+client
+  .connect()
+  .then((client) => db = client.db(process.env.MONGODB_DB_NAME))
+  .catch((err) => console.log('connection error:', err));
+
+const findAll = async (collection) => {
   try {
-    await client.connect();
-    const db = client.db(process.env.MONGODB_DB_NAME);
-    const document = await db.collection(collection).find({}).toArray();
-    return document;
+    const documents = await db.collection(collection).find({}).toArray();
+    return documents;
   } catch (err) {
     console.log(err.stack);
-  } finally {
-    await client.close();
   }
 };
 
 const create = async (collection, body) => {
   try {
-    await client.connect();
-    const db = client.db(process.env.MONGODB_DB_NAME);
-    await db.collection(collection).insertOne(body);
+    const col = db.collection(collection);
+    await col.insertOne(body);
   } catch (err) {
     console.log(err.stack);
-  } finally {
-    await client.close();
   }
 };
 
 const del = async (collection, id) => {
   try {
-    await client.connect();
-    const db = client.db(process.env.MONGODB_DB_NAME);
-    await db.collection(collection).deleteOne(id);
+    const _id = new mongodb.ObjectID(id);
+    await db.collection(collection).deleteOne({ _id: _id });
   } catch (err) {
     console.log(err.stack);
-  } finally {
-    await client.close();
   }
 };
 
-export { find, create, del };
+export { findAll, create, del };
